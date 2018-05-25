ea558829a20c65eb551cdcea62c5740b972d8b42
sr354095 <sr354095@wipro.com>
2018-05-25 14:51:11 +0530

    responsiveness added

--- client/views/homePage.jsx
+++ client/views/homePage.jsx
@@ -1,4 +1,7 @@
 import React,{Component} from 'react';
+
+import { Grid } from 'semantic-ui-react'
+
 import AppBar from './../components/appBar.jsx'
 
 export default class HomePage extends Component
@@ -6,7 +9,13 @@ export default class HomePage extends Component
   render()
   {
     return(
-      <AppBar />
+      <Grid>
+        <Grid.Row only='large screen'>
+          <Grid.Column>
+            <AppBar />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
